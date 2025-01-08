17edab5351dedc4769a0f5af263cef49fe3a31c2
mimorisuzuko <hrky0206@gmail.com>
2025-01-08 23:17:55 +0900

    ESLint

--- examples/yuuka/eslint.config.mjs
+++ examples/yuuka/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +10,30 @@ const compat = new FlatCompat({
 	baseDirectory: __dirname
 });
 
-const eslintConfig = [
-	...compat.extends("next/core-web-vitals", "next/typescript")
-];
-
-export default eslintConfig;
+export default tseslint.config(
+	...compat.extends("next/core-web-vitals"),
+	tseslint.configs.recommendedTypeChecked,
+	{
+		rules: {
+			"@typescript-eslint/no-explicit-any": "off",
+			"@typescript-eslint/no-unnecessary-type-assertion": "off",
+			"@typescript-eslint/strict-boolean-expressions": [
+				"error",
+				{ allowString: false, allowNumber: false, allowNullableObject: false }
+			],
+			"@typescript-eslint/no-floating-promises": [
+				"error",
+				{
+					ignoreIIFE: true
+				}
+			]
+		},
+		ignores: ["eslint.config.mjs"],
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname
+			}
+		}
+	}
+);
